5f7916e1388bbe8c21408367833e77bf3877a444
Jameson Taylor <jameson@ionic.io>
2022-07-21 14:12:21 -0400

    Add styles to position button correctly;

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild(IonModal) modal: IonModal;
 
   constructor() {}
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
 
 }
