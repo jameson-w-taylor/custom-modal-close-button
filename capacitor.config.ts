603e1470bfa373c5be3a67fc3c43b7b70992c542
Jameson Taylor <jameson@ionic.io>
2022-07-21 13:14:56 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'custom-modal-close-button',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
